8b84ecba04a7ebd7b02296b8cfee3451dd4409ab
tkmaher <tkmaher@umich.edu>
2026-01-04 15:49:17 -0600

    fr

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import EditPage from "@/src/components/edit";
 
 export default function Page() {
     return (
-        <div style={{marginLeft: "25%", marginRight: "25%", width: "100%"}}>
+        <div style={{marginLeft: "25%", marginRight: "25%"}}>
             <EditPage/>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,48 @@
+"use client";
 import Link from "next/link";
 import Info from "@/src/components/info";
 import { EmailPoster } from "@/src/components/edit";
+import { useState, useRef } from "react";
 
 
 export default function Home() {
-  
+  const [modCount, setModcount] = useState(0);
+
+  const style = {
+    transform: `translate(${(1 - Math.abs((modCount % 4) - 2)) * 100}%, 0%)`
+  }
 
   return (
-    <div>
-      <div className="flex-body">
-      <Info/>
-        <div className="link-flex">
-          <span><Link href={"/archive"}>Tickets</Link> ▪</span>
-          <span><a target="_blank" href={"mailto:blipppppppppppp@gmail.com"}>Contact</a> ▪</span>
+    <>
+      <div id="inverting-overlay" style={style}>
+        <div></div>
+      </div>
+    
+      <div className="homepage">
+      
+        <div className="flex-body">
+        <Info/>
+          <div className="link-flex">
+            <span><Link href={"/archive"}>Tickets</Link> ▪</span>
+            <span><a target="_blank" href={"mailto:blipppppppppppp@gmail.com"}>Contact</a> ▪</span>
 
+          </div>
         </div>
-      </div>
-      <div id="bronze">
-        <img src="/temp.png"/>
-      </div>
-      <div className="flex-body">
-        <div className="link-flex">
+        
 
-          <span><a target="_blank" href={"/archive"}>Archive</a> ▪</span>
+        <div id="footer">
+          <div className="flex-body">
+            <div className="link-flex">
 
+              <span><a target="_blank" href={"/archive"}>Archive</a> ▪</span>
+              <span><a onClick={() => setModcount(modCount+1)}>?</a> ▪</span>
+
+            </div>
+          </div>
+          <EmailPoster admin={false}/>
         </div>
-      </div>
-      <EmailPoster admin={false}/>
 
-    </div>
+      </div>
+    </>
   );
 }
